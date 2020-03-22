4b2660a24d6d7d6587774281ed1d07ecaccb333c
Johan L <54843571+Jolu1806@users.noreply.github.com>
2020-03-22 20:48:11 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<!-- Anslutning till DB -->
+
+<?php
+    error_reporting(-1);
+    ini_set("display_errors" , 1);
+    
+    //Inställningar för databas lokalt
+    define("DBHOST", "studentmysql.miun.se");
+    define("DBUSER", "jolu1806");
+    define("DBPASSWORD", "7mp3y0ft");
+    define("DBDATABASE", "jolu1806");
+
+    
